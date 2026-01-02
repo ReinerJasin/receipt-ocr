9cf41ed548ce0e9cee0768f993b968daa4a6075b
ReinerJasin <62686723+ReinerJasin@users.noreply.github.com>
2026-01-02 23:31:21 +0800

    Update inference.py

--- project/inference.py
+++ project/inference.py
@@ -1,1 +1,9 @@
+# command to install packages
+# pip install -U "paddleocr[doc-parser]"
+# pip install paddlepaddle==3.2.1
 
+from paddleocr import PaddleOCRVL
+
+pipeline = PaddleOCRVL(
+    device="cpu"
+)
