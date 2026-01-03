f60fd7ed161f3d909ec357ae16b93d10975359c7
ReinerJasin <62686723+ReinerJasin@users.noreply.github.com>
2026-01-03 23:58:34 +0800

    inference (not finished yet)

--- project/inference.py
+++ project/inference.py
@@ -5,5 +5,13 @@
 from paddleocr import PaddleOCRVL
 
 pipeline = PaddleOCRVL(
-    device="cpu"
+    use_doc_orientation_classify=False,
+    use_doc_unwarping=False,
+    use_layout_detection=True
 )
+
+output = pipeline.predict("/Users/reiner/Downloads/sample_receipt.jpeg")
+
+for res in output:
+    res.print()                 # debug view
+    structured = res.json       # structured output
